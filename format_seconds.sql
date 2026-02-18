1bcb812c7fa9842ab8f5fde4ffb829c9e708da60
szymonw <handerdog3@gmail.com>
2026-02-18 20:37:58 +0100

    function for querying pretty time format

---
+++ format_seconds.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION format_seconds(seconds BIGINT)
+RETURNS TEXT AS $$
+DECLARE
+	hours INTEGER;
+	mins INTEGER;
+	secs INTEGER;
+BEGIN
+	IF seconds IS NULL THEN
+		RETURN NULL;
+	END IF;
+
+	hours := (seconds / 3600);
+	mins := ((seconds % 3600) / 60);
+	secs := (seconds % 60);
+
+	RETURN hours || 'h ' ||
+		lpad(mins::text, 2, '0') || 'm ' ||
+		lpad(secs::text, 2, '0') || 's ';
+END;
+$$ LANGUAGE PLPGSQL IMMUTABLE;
+		
